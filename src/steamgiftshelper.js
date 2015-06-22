7c467845fb26cb1d287916a2fc7b749cb09a7c6e
seeeeew <for-github@nyox.de>
2015-06-22 15:17:28 +0200

    fixed entering giveaways

--- src/steamgiftshelper.js
+++ src/steamgiftshelper.js
@@ -1,6 +1,8 @@
 $(function(){
 	"use strict";
 	
+	var xsrf_token = $("[name=xsrf_token]")[0].value;
+	
 	// restore scroll position after hiding game
 	var scroll = window.location.hash.match(/^#?(\d+)/);
 	if (scroll) {
@@ -17,7 +19,6 @@ $(function(){
 	$("a.giveaway__icon[href*='//store.steampowered.com/app/']").each(function() {
 		var match = this.href.match(/^[^:]+:\/\/store.steampowered.com\/app\/(\d+)/);
 		var appid = match[1];
-		add_enter_icon(this);
 		$.ajax({
 			url: "http://store.steampowered.com/api/appdetails/?filters=platforms&appids=" + appid,
 			context: this
@@ -26,10 +27,9 @@ $(function(){
 			add_platform_icons(this, data[appid].data.platforms);
 		})
 	});
-	
-	function add_enter_icon(element) {
+	$("a.giveaway__icon[href*='//store.steampowered.com/app/'], a.giveaway__icon[href*='//store.steampowered.com/sub/']").each(function() {
 		var enter_icon = $.parseHTML("<i class='giveaway__icon fa'></i>")[0];
-		element.parentNode.insertBefore(enter_icon, element);
+		this.parentNode.insertBefore(enter_icon, this);
 		enter_icon.onclick = click_enter_icon;
 		enter_icon.parentWrapper = $(enter_icon.parentNode.parentNode.parentNode);
 		if (enter_icon.parentWrapper.hasClass("is-faded")) {
@@ -37,10 +37,9 @@ $(function(){
 		} else {
 			$(enter_icon).addClass("fa-plus-circle");
 		}
-	}
+	});
 	function click_enter_icon() {
 		var code = (this.parentNode.firstElementChild.href || {}).match(/\/giveaway\/([^\/\?]+)/);
-		var xsrf_token = $("[name=xsrf_token]")[0].value;
 		var entered = this.parentWrapper.hasClass("is-faded");
 		if (code) {
 			$.ajax({
