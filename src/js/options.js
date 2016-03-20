be537f881809a1a86880b2b2c28180087c4e679c
seeeeew <for-github@nyox.de>
2016-03-20 01:34:29 +0100

    minor code structure changes

--- src/js/options.js
+++ src/js/options.js
@@ -1,16 +1,25 @@
-var storage = chrome.storage.sync;
+"use strict";
 
-$(storage.get(function(settings) {
-	$("input[type=checkbox]").each(function() {
-		if (!settings.hasOwnProperty(this.id)) {
-			settings[this.id] = true;
-		}
-		this.checked = settings[this.id];
-		this.onchange = function() {
-			var save = new Object();
-			save[this.id] = this.checked;
-			storage.set(save);
-		}
-	});
+var storage_sync = chrome.storage.sync;
+var storage_sync_deferred = new $.Deferred();
+storage_sync.get(storage_sync_deferred.resolve);
+var storage_local = chrome.storage.local;
+var storage_local_deferred = new $.Deferred();
+storage_local.get(storage_local_deferred.resolve);
+
+$($.when(storage_sync_deferred, storage_local_deferred).done(function(settings, cache) {
+
+$("input[type=checkbox]").each(function() {
+	if (!settings.hasOwnProperty(this.id)) {
+		settings[this.id] = true;
+	}
+	this.checked = settings[this.id];
+	this.onchange = function() {
+		var save = new Object();
+		save[this.id] = this.checked;
+		storage_sync.set(save);
+	}
+});
+
 }));
 
