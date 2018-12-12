b7c1f6adf82b11753132ead1228cbedddad15571
seeeeew <for-github@nyox.de>
2018-12-12 01:23:54 +0100

    general code cleanup

--- src/steamgiftshelper.js
+++ src/steamgiftshelper.js
@@ -26,18 +26,31 @@ Promise.all([storage_sync_promise, storage_local_promise, document_ready_promise
 settings = {...defaultsettings, ...settings};
 
 // reset cache after update
-var version = chrome.runtime.getManifest().version;
+const version = chrome.runtime.getManifest().version;
 if (version !== cache.version) {
 	chrome.storage.local.clear();
 	cache = {version: version};
 	chrome.storage.local.set(cache);
 }
 
+function requestJSON(method, url, parameters) {
+	return new Promise((resolve) => {
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState != 4 || xhr.status != 200) return;
+			resolve(JSON.parse(xhr.responseText));
+		};
+		xhr.open(method, url);
+		if (method === "POST") xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.send(parameters);
+	});
+}
+
 // platform support cache
-var platforms = (function(cache) {
+const Platforms = (function(cache) {
 	
 	function cache_clean() {
-		var expire_time = parseInt(Date.now() / 1000, 10) - 86400; // one day ago
+		const expire_time = parseInt(Date.now() / 1000, 10) - 86400; // one day ago
 		for (const [key, values] of Object.entries(cache)) {
 			if (values[1] < expire_time) {
 				delete cache[key];
@@ -56,7 +69,7 @@ var platforms = (function(cache) {
 			if (cache[id] !== undefined) {
 				resolve(cache[id][0]);
 			} else {
-				var [type, itemid] = id.split("/", 2), url;
+				let [type, itemid] = id.split("/", 2), url;
 				switch(type) {
 					case "app":
 						url = "//store.steampowered.com/api/appdetails/?filters=platforms&appids=" + itemid;
@@ -68,29 +81,24 @@ var platforms = (function(cache) {
 						reject();
 						return;
 				}
-				var xhr = new XMLHttpRequest();
-				xhr.onreadystatechange = function() {
-					if (xhr.readyState != 4 || xhr.status != 200) return;
-					var data = JSON.parse(xhr.responseText);
+				requestJSON("GET", url).then((data) => {
 					if (((data[itemid] || {}).data || {}).platforms) {
-						var platforms = data[itemid].data.platforms;
+						const platforms = data[itemid].data.platforms;
 						cache_set(id, platforms);
 						resolve(platforms);
 					}
-				};
-				xhr.open("GET", url);
-				xhr.send();
+				});
 			}
 		});
 	}
 	
 	return {
-		get: get
+		get
 	};
 }(cache.platforms || {}));
 
 // synchronize points across tabs
-var previous_points = document.querySelector(".nav__points").innerText;
+let previous_points = document.querySelector(".nav__points").innerText;
 function updatePoints(points, synchronize) {
 	if (points != previous_points) {
 		previous_points = points;
@@ -111,8 +119,8 @@ if (settings.synchronize_points) {
 		}
 	});
 	document.querySelector(".nav__points").addEventListener("DOMSubtreeModified", (event) => {
-		var points = event.target.innerText;
-		if (points != "" && points != previous_points) {
+		const points = event.target.innerText;
+		if (points !== "" && points !== previous_points) {
 			synchronizePoints(points);
 			previous_points = points;
 		}
@@ -135,50 +143,47 @@ document.querySelectorAll(".sidebar__search-container .fa-search").forEach((elem
 if (window.location.pathname.match(/^\/(?:$|giveaways\/)/)) {
 	// Add platform support icons
 	if (settings.platform_icons) {
-		var add_platform_icons = function(element, platforms) {
-			var next = element.nextSibling;
+		const add_platform_icons = function(element, platforms) {
+			const next = element.nextSibling;
 			if (platforms.windows) {
-				var icon = document.createElement("i");
+				const icon = document.createElement("i");
 				icon.classList.add("giveaway__icon", "fa", "fa-windows");
 				next.parentNode.insertBefore(icon, next);
 			}
 			if (platforms.mac) {
-				var icon = document.createElement("i");
+				const icon = document.createElement("i");
 				icon.classList.add("giveaway__icon", "fa", "fa-apple");
 				next.parentNode.insertBefore(icon, next);
 			}
 			if (platforms.linux) {
-				var icon = document.createElement("i");
+				const icon = document.createElement("i");
 				icon.classList.add("giveaway__icon", "fa", "fa-linux");
 				next.parentNode.insertBefore(icon, next);
 			}
 		};
 		
 		document.querySelectorAll("a.giveaway__icon[href*='//store.steampowered.com/']").forEach((element) => {
-			var match = element.href.match(/^[^:]+:\/\/store.steampowered.com\/((?:app|sub)\/\d+)/);
+			const match = element.href.match(/^[^:]+:\/\/store.steampowered.com\/((?:app|sub)\/\d+)/);
 			if (match) {
-				platforms.get(match[1]).then((platforms) => {
+				Platforms.get(match[1]).then((platforms) => {
 					add_platform_icons(element, platforms);
 				});
 			}
 		});
 	}
 	
-	var xsrf_token = document.querySelector("[name=xsrf_token]").value;
+	const xsrf_token = document.querySelector("[name=xsrf_token]").value;
 	if (xsrf_token) {
 		if (settings.enter_button) {
-			var click_enter_icon = function(event) {
-				var enter_icon = event.target;
-				var code = enter_icon.parentElement.firstElementChild.href.match(/\/giveaway\/([^\/\?]+)/);
-				var wrapper = enter_icon.closest(".giveaway__row-inner-wrap");
-				var entered = wrapper.classList.contains("is-faded");
+			const click_enter_icon = function(event) {
+				const enter_icon = event.target;
+				const code = enter_icon.parentElement.firstElementChild.href.match(/\/giveaway\/([^\/\?]+)/);
+				const wrapper = enter_icon.closest(".giveaway__row-inner-wrap");
+				const entered = wrapper.classList.contains("is-faded");
 				enter_icon.classList.remove("fa-plus-circle", "fa-minus-circle");
 				enter_icon.classList.add("fa-spin", "fa-refresh");
-				var xhr = new XMLHttpRequest();
-				xhr.open("POST", "/ajax.php");
-				xhr.onreadystatechange = (event) => {
-					if (xhr.readyState != 4 || xhr.status != 200) return;
-					var data = JSON.parse(xhr.responseText);
+				const parameters = "xsrf_token=" + encodeURIComponent(xsrf_token) + "&do=entry_" + (entered ? "delete" : "insert") + "&code=" + encodeURIComponent(code[1]);
+				requestJSON("POST", "/ajax.php", parameters).then((data) => {
 					enter_icon.classList.remove("fa-spin", "fa-refresh");
 					if (data.type === "success") {
 						if (wrapper.classList.contains("is-faded")) {
@@ -196,15 +201,13 @@ if (window.location.pathname.match(/^\/(?:$|giveaways\/)/)) {
 						}
 					}
 					updatePoints(data.points);
-				};
-				xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-				xhr.send("xsrf_token=" + encodeURIComponent(xsrf_token) + "&do=entry_" + (entered ? "delete" : "insert") + "&code=" + encodeURIComponent(code[1]));
+				});
 			};
 		
 			document.querySelectorAll("a.giveaway__icon[href*='//store.steampowered.com/app/'], a.giveaway__icon[href*='//store.steampowered.com/sub/']").forEach((element) => {
 				// Enable entering giveways from browsing page
 				if (settings.enter_button) {
-					var enter_icon = document.createElement("i");
+					const enter_icon = document.createElement("i");
 					enter_icon.classList.add("giveaway__icon", "fa");
 					element.parentNode.insertBefore(enter_icon, element);
 					enter_icon.addEventListener("click", click_enter_icon);
